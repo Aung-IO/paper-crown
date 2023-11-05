8d13af4c1a45a15c3ca958dfe05d307e2e73d563
Aung Pyae Kyaw <aungpyaekyawwiz242@gmail.com>
2023-11-05 17:05:17 +0630

    add creative process img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,9 @@ function Home() {
               />
 
               <div className="absolute bottom-20 text-white text-xl mx-36 cursor-pointer">
-                <Link to="/books" className=" text-white hover:underline">Books</Link>
+                <Link to="/books" className=" text-white hover:underline">
+                  Books
+                </Link>
               </div>
             </div>
           </li>
@@ -48,6 +50,30 @@ function Home() {
             </div>
           </li>
         </ul>
+      </div>
+
+      <div className="relative mt-10 bg-slate-200">
+        <img
+          src="../src/assets/cover1.jpg"
+          className="object-cover mix-blend-multiply w-full"
+        />
+
+        <div className="absolute text-white inset-0 flex items-center justify-center">
+          <ul className="space-y-3">
+            <li className=" text-3xl font-sans">
+              <p>PAPER CROWN ARTISTS</p>
+            </li>
+            <li className="mx-10 text-lg">
+              <p>Click to view creative process</p>
+            </li>
+
+            <li className="mx-24">
+              <button class="bg-white hover:bg-black text-black font-semibold hover:text-white py-2 px-4 border-2 border-black  rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                LEARN MORE
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
