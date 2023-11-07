f0159233a9a9bf1bd23ac7b73203f698e41af87d
Aung Pyae Kyaw <aungpyaekyawwiz242@gmail.com>
2023-11-08 00:39:54 +0630

    add page transition

--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 import Navbar from "../../components/Navbar";
+import './style.css';
 
 export default function Layout() {
+  const location = useLocation()
+
   return (
     <div>
       <Navbar />
 
       {/* dynamic route changes */}
-      <div className="mt-3 ">
-        <Outlet />
-      </div>
+      <SwitchTransition>
+        <CSSTransition timeout={200} classNames='fade' key={location.pathname}>
+          <div className="mt-3 ">
+            <Outlet />
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
 
       {/* <Footer /> */}
     </div>
