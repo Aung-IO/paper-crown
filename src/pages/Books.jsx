80c2eef924fcc01bd10e6fc871201541d2321a9f
Aung Pyae Kyaw <aungpyaekyawwiz242@gmail.com>
2023-11-08 00:40:12 +0630

    filter setup

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -5,20 +5,21 @@ export default function Books() {
   return (
     <>
       <div className="h-40 w-full bg-zinc-50">
-        <div className="text-5xl text-center items-center p-14 font-serif">
+        <div className="text-5xl text-center items-center p-14 font-mono">
           <span>Books</span>
         </div>
 
         <div>
-          <ul className="list-none p-0 m-0 flex">
+          <ul className="flex m-4">
             <li className="w-1/5 p-3 border border-red-600">
               {/* Filters by authors */}
-              <h1>Nova</h1>
-              <h1>Soka</h1>
-              <h1>AK</h1>
-              <h1>Siae Saing</h1>
-              <h1>War Pu</h1>
-              <h1>Yoking</h1>
+             <p className="text-sm text-gray-400 font-mono border-b-2 p-1">FILTERS</p>
+             <div>
+                <p>Kim Jung Gi</p>
+                <p>Dong Ho Kim</p>
+                <p>Hoooook</p>
+                <p>Peter Han</p>
+             </div>
             </li>
             <li className="w-4/5">
               <BookList />
