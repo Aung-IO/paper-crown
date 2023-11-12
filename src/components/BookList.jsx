a9041f89acc419477aed6061431aae8e848371d7
Aung Pyae Kyaw <aungpyaekyawwiz242@gmail.com>
2023-11-12 22:19:41 +0630

    add search feature

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import cover from "../assets/book_cover.png";
 import useFetch from "../hooks/useFetch";
 
 function BookList() {
-  let { data: books, loading, error } = useFetch("http://localhost:3000/books");
+  let location = useLocation();
+  let param = new URLSearchParams(location.search);
+  let search = param.get("search");
+  let {
+    data: books,
+    loading,
+    error,
+  } = useFetch(`http://localhost:3000/books${search ? `?q=${search}` : ""}`);
 
   if (error) {
     return <p>{error}</p>;
@@ -12,21 +19,20 @@ function BookList() {
   return (
     <div>
       {loading && <p>loading ... </p>}
-    
+
       {/* book list */}
       {!!books && (
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-3" >
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-3">
           {books.map((book) => (
             <Link to={`/books/${book.id}`} key={book.id}>
-             <div className="p-4">
-              <img src={cover} alt="" />
-              <div className="text-center space-y-2 mt-3">
-                <h1>{book.title}</h1>
-                <p>{book.price}</p>
+              <div className="p-4">
+                <img src={cover} alt="" />
+                <div className="text-center space-y-2 mt-3">
+                  <h1>{book.title}</h1>
+                  <p>{book.price}</p>
+                </div>
               </div>
-            </div>
             </Link>
-           
           ))}
         </div>
       )}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import CreateBook from "./CreateBook";
 
 export default function Navbar() {
   let navigate = useNavigate();
@@ -8,6 +7,14 @@ export default function Navbar() {
     let path = "/create";
     navigate(path);
   };
+
+  let [search, setSearch] = useState("");
+
+  let handleSearch = () => {
+    navigate("/books/?search=" + search);
+    setSearch('');
+  };
+
   return (
     <nav>
       <ul className="flex justify-around mt-4 ">
@@ -60,53 +67,31 @@ export default function Navbar() {
                 </svg>
               </button>
             </li>
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                />
-              </svg>
-            </li>
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                />
-              </svg>
-            </li>
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                />
-              </svg>
+
+            <li className="flex items-center gap-2">
+              <input
+                type="text"
+                placeholder="search books..."
+                className="outline-none"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button onClick={handleSearch}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  />
+                </svg>
+              </button>
             </li>
           </ul>
         </li>
