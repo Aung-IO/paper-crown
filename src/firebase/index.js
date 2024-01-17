139e6d4d824873db1faaf9e1249cf30ad18d08ee
Aung Pyae Kyaw <aungpyaekyawwiz242@gmail.com>
2024-01-17 09:54:29 +0630

    firebase resetup

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBPt3spCWLCUU_XJ7qYmJlpixPIRHm5FKE",
-  authDomain: "paper-crown-48ae3.firebaseapp.com",
-  projectId: "paper-crown-48ae3",
-  storageBucket: "paper-crown-48ae3.appspot.com",
-  messagingSenderId: "147470463418",
-  appId: "1:147470463418:web:ca2c3217a984c280e26d83",
-  measurementId: "G-643YM63MT3",
+  apiKey: "AIzaSyBYpuqg8us4ktkjgE4zDTDkqElZ6v8y2OU",
+  authDomain: "paper-crown-proj.firebaseapp.com",
+  projectId: "paper-crown-proj",
+  storageBucket: "paper-crown-proj.appspot.com",
+  messagingSenderId: "946593690259",
+  appId: "1:946593690259:web:1e755cb71af1962d7c5366",
+  measurementId: "G-KJPCS6X663"
 };
 
 const app = initializeApp(firebaseConfig);
